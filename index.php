8528f76da014676e2e981109c7332f1704f939a2
InsertIntoHTL <5125@htl.rennweg.at>
2020-09-03 12:24:28 +0200

    magnetic buttons

--- index.php
+++ index.php
@@ -14,11 +14,6 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-    <!--Barba.js-->
-    <!-- unpkg -->
-    <script src="https://unpkg.com/@barba/core"></script>
-    <!-- jsdelivr -->
-    <script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
 
     <!-- Fontawesome -->
     <link rel="stylesheet" href="./resources/fontawesome-free-5.14.0-web/css/all.css" />
@@ -315,14 +310,14 @@
     </main>
 
 	<svg class="cursor" width="25" height="25" viewBox="0 0 25 25">
-		<circle class="cursor__inner" cx="12.5" cy="12.5" r="6.25" fill="green"></circle>
+		<circle class="cursor__inner" cx="12.5" cy="12.5" r="6.25"></circle>
 	</svg>
+	<script src="js/demo.9ce5c2ea.js"></script>
+	<script src="js/demo1.151408fb.js"></script>
 
     <script type="text/javascript" src="js/form.js"></script>
     <script type="module" src="js/spheres.js"></script>
 
-	<script src="js/demo.9ce5c2ea.js"></script>
-	<script src="js/demo1.151408fb.js"></script>
 </body>
 
 </html>
